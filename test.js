135981d4bc8c80fff65a65807b248018a4c81b38
Sushmita Yadav <yadavsushmita293@gmail.com>
2024-01-03 11:53:15 +0530

    adding all the file

---
+++ test.js
@@ -0,0 +1,17 @@
+//  ****************Operations****************
+
+let str1 = "HariBol"
+// let str2  ="Bol"
+// console.log(str1 + str2);ll
+
+// console.log(str1.length);
+// console.log(str1.endsWith("Bol"));
+// console.log(str1.indexOf('o'));
+// console.log(str1.replace('B', 'b'));
+// console.log(str1.split(""));
+
+let myDate = new Date()
+console.log(myDate.getDate());
+console.log(myDate.getMonth() + 1);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
